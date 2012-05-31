de208d987f9183d14f2cc8246d4e9c50354b3fb0
CinnamonTeal <cinnamon_teal@fastmail.fm>
2012-05-31 19:27:22 -0400

    get friends and similar friends

--- lib/twitter_scraper.rb
+++ lib/twitter_scraper.rb
@@ -1,15 +1,41 @@
 require "twitter_scraper/version"
 require "httpclient"
 require "json"
-
+require "pry"
 module TwitterScraper
-	def self.get_user(screen_name)
-		JSON[client.get("http://twitter.com/users/show_for_profile.json?screen_name=#{screen_name}").body]
-	end
 
-	private
+	class << self
+		def get_user(screen_name)
+			JSON[client.get("http://twitter.com/users/show_for_profile.json?screen_name=#{screen_name}").body]
+		end
 
-	def self.client
-		@@client ||= HTTPClient.new
+		def get_friends(screen_name)
+			ids = JSON[client.get("http://api.twitter.com/1/friends/ids.json?screen_name=#{screen_name}").body]["ids"]
+			users = {}
+			ids.each_slice(100) do |ids_slice|
+				JSON[client.get("http://api.twitter.com/1/users/lookup.json?user_id=#{ids_slice.join(',')}").body].each do |user|
+					users[user["screen_name"]] = user
+				end
+			end
+			users
+		end
+
+		def get_followers(screen_name)
+			ids = JSON[client.get("http://api.twitter.com/1/followers/ids.json?screen_name=#{screen_name}").body]["ids"]
+			users = JSON[client.get("http://api.twitter.com/1/users/lookup.json?user_id=#{ids.join(',')}").body]
+		end
+
+		def get_similar_friends(source_screen_name, target_screen_name)
+			source_friends = get_friends(source_screen_name)
+			target_friends = get_friends(target_screen_name)
+			shared_friends = source_friends.keys & target_friends.keys
+			source_friends.values_at(*shared_friends)
+		end
+
+		private
+
+		def client
+			@@client ||= HTTPClient.new
+		end
 	end
 end
--- test/test_twitter_scraper.rb
+++ test/test_twitter_scraper.rb
@@ -4,7 +4,22 @@ class TwitterScraperTest < Test::Unit::TestCase
 
 	def test_get_user_date
 		user = TwitterScraper.get_user("ryanmong")
-		print user.inspect
 		assert_equal("ryanmong", user["user"]["screen_name"])
+	end
+
+	def test_get_followers
+		followers = TwitterScraper.get_followers("ryanmong")
+		assert(followers.size > 0)
+	end
+
+	def test_get_friends
+		friends = TwitterScraper.get_friends("ryanmong")
+		assert(friends.size > 0)
+	end
+
+	def test_get_similar_friends
+		similar_friends = TwitterScraper.get_similar_friends("ryanmong","binghott")
+		andy = similar_friends.select { |user| user["screen_name"] == "AndySecretStash" }.first
+		assert(andy)
 	end
 end
